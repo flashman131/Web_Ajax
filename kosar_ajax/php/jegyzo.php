cd1b7bd85dad0fd3a4dcd7d368a903ca36e5a10c
Balázs Petró <flashman131@gmail.com>
2019-02-13 20:01:10 +0100

    asd

--- kosar_ajax/php/jegyzo.php
+++ kosar_ajax/php/jegyzo.php
@@ -2,11 +2,28 @@
 require_once('../config/connect.php');
 if(isset($_POST['mez'])){
     $mez=$_POST['mez'];
-    $sql="SELECT * FROM jegyzokonyv WHERE mez='$mez';";
+    $sql="SELECT be,ki,bkis,bjo FROM jegyzokonyv WHERE mez='$mez';";
     $res = $conn -> query($sql);
     if(!$res){
         die("Hiba a lekérdezés során!");
     }
+    /*if(res->num_rows >0){
+     * $html = "<table class='table table-striped'>"
+     *  ."<tr>"
+     *  ."<td> Be </td>"
+     *  ."<td> Ki </td>"
+     *  ."<td> Kísérlet </td>"
+     *  ."<td> Kosár </td>"
+     *  ."</tr>";
+     * }     
+        while($row=$res ->fetch_assoc()){
+        $html .= "<tr>"
+     *      . "<td> {$row['be']} </tr>"
+            . "<td> {$row['ki']} </tr>"
+            . "<td> {$row['bkis']} </tr>"
+            . "<td> {$row['bjo']} </tr>"
+            . "</tr>";
+     * }        */
     while($row=$res ->fetch_assoc()){
         $html="<ul>";
         $html .= "<li> {$row['be']} </li>"
